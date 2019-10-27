cdc1f505ec1c50ac18aa54f8250c19ffef66c877
Akhil Indurti <aindurti@gmail.com>
2019-10-27 15:09:59 -0400

    Preliminary application structure (#9)
    This provides a preliminary application structure that defines a template and static directory. This should serve as a foundation for subsequent changes.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 import mysql.connector as db
 import logging
-app = Flask("pokemonstay")
+app = Flask(__name__)
 
 # Store application's state in this dictionary
 stay = {}
@@ -15,17 +15,21 @@ def env(s):
 
 def init():
     # Use this flag for debug-specific logic
-    stay.debug = env("DEBUG") == "True"
-    stay.conn = db.connect(
+    stay["debug"] = env("DEBUG") == "True"
+    stay["conn"] = db.connect(
         host=env("DB_HOST"),
         user=env("DB_USERNAME"),
         password=env("DB_PASSWORD"),
         database=env("DB_NAME"),
     )
+    app = Flask(
+        import_name="pokemonstay",
+        static_url_path="/static",
+    )
 
 @app.route("/")
 def root():
-    return "Hello Peeps"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     init()
