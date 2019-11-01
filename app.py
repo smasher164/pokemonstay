5355bb9ad9fb270aef7ea3edd9a085e0d3cf0385
smasher164 <aindurti@gmail.com>
2019-10-31 20:53:51 -0400

    fix template parameter for index

--- app.py
+++ app.py
@@ -333,5 +333,5 @@ def auth():
 def root():
     token = authenticate(request.cookies.get('access_token'))
     if token is not None:
-        return render_template("index.html", token)
+        return render_template("index.html", token=token)
     return render_template("auth.html")
