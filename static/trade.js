1efa2340ce1e484241691164c520e62ac5ad708d
smasher164 <aindurti@gmail.com>
2019-11-17 22:48:00 -0500

    Major UI updates
    Update Navbar
    Add Footer
    Update trade page
    Fix margin, padding, spacing issues everywhere

--- static/trade.js
+++ static/trade.js
@@ -1,3 +1,6 @@
+const magenta = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mP8OnPHfwAIGANHqdc/SQAAAABJRU5ErkJggg=="
+const blue = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mOsuvWsHgAGywK7a9uC3wAAAABJRU5ErkJggg=="
+
 let btnJoin = document.querySelector("#btn-join")
 let btnLeave = document.querySelector("#btn-leave")
 let btnConfirm = document.querySelector("#btn-confirm")
@@ -6,6 +9,8 @@ let user1 = document.querySelector("#user1")
 let user2 = document.querySelector("#user2")
 let pokemon1 = document.querySelector("#pokemon1")
 let pokemon2 = document.querySelector("#pokemon2")
+let tradeimg1 = document.querySelector("#tradeimg1")
+let tradeimg2 = document.querySelector("#tradeimg2")
 let confirm1 = document.querySelector("#confirm1")
 let confirm2 = document.querySelector("#confirm2")
 let selectBox = document.querySelector("#select-box")
@@ -20,17 +25,68 @@ trade = {}
 box = []
 options = `<option value=""></option>`
 
+let pad = num => num <= 999 ? `00${num}`.slice(-3) : num
+
+let pokeimg = (num, shiny) => {
+    if (shiny) {
+        return `https://www.serebii.net/Shiny/SM/${pad(num)}.png`
+    }
+    return `https://www.serebii.net/sunmoon/pokemon/${pad(num)}.png`
+}
+
+let gender = {
+    0: "Genderless",
+    1: "Male",
+    2: "Female",
+}
+
+let lv = n => {
+    if (n == null) {
+        return ""
+    }
+    return "Lv"+n
+}
+
+let name = (species, nickname, shiny) => {
+    let s = species
+    if (nickname) {
+        s += "("+nickname+")"
+    }
+    if (shiny) {
+        s += "★"
+    }
+    return s
+}
+
 let updateUI = () => {
     btnJoin.style.display = joinable ? "" : "none"
     btnLeave.style.display = leavable ? "" : "none"
-    user1.textContent = "User 1: " + (trade.user1Id ? trade.user1Id : "")
-    user2.textContent = "User 2: " + (trade.user2Id ? trade.user2Id : "")
-    pokemon1.textContent = "Pokémon ID 1: " + (trade.pokemon1 ? trade.pokemon1 : "")
-    pokemon2.textContent = "Pokémon ID 2: " + (trade.pokemon2 ? trade.pokemon2 : "")
-    confirm1.textContent = "Confirm 1: " + Boolean(trade.confirm1)
-    confirm1.style.display = trade.user1Id ? "" : "none"
-    confirm2.textContent = "Confirm 2: " + Boolean(trade.confirm2)
-    confirm2.style.display = trade.user2Id ? "" : "none"
+    user1.textContent = trade.userName1 ? `User 1: ${trade.userName1}` : "Waiting for User 1..."
+    user2.textContent = trade.userName2 ? `User 2: ${trade.userName2}` : "Waiting for User 2..."
+    if(trade.pokemon1 && trade.pokemon1.ownsId != null) {
+        pokemon1.textContent = [
+            name(trade.pokemon1.species, trade.pokemon1.nickname, trade.pokemon1.shiny),
+            gender[trade.pokemon1.gender],
+            lv(trade.pokemon1.level)
+        ].join("\n")
+        tradeimg1.src = pokeimg(trade.pokemon1.number, trade.pokemon1.shiny)
+    } else {
+        pokemon1.textContent = "No offer"
+        tradeimg1.src = magenta
+    }
+    if(trade.pokemon2 && trade.pokemon2.ownsId != null) {
+        pokemon2.textContent = [
+            name(trade.pokemon2.species, trade.pokemon2.nickname, trade.pokemon2.shiny),
+            gender[trade.pokemon2.gender],
+            lv(trade.pokemon2.level)
+        ].join("\n")
+        tradeimg2.src = pokeimg(trade.pokemon2.number, trade.pokemon2.shiny)
+    } else {
+        pokemon2.textContent = "No offer"
+        tradeimg2.src = blue
+    }
+    confirm1.textContent = Boolean(trade.confirm1) ? 'Confirmed' : "Not confirmed"
+    confirm2.textContent = Boolean(trade.confirm2) ? 'Confirmed' : "Not confirmed"
     selectBox.disabled = !leavable
     if (selectBox.disabled) {
         options = ""
@@ -51,17 +107,13 @@ let updateUI = () => {
     } else if (confirmable) {
         btnConfirm.style.display = ""
         btnCancel.style.display = "none"
-    } else {
+    } else if (leavable) {
         btnConfirm.style.display = "none"
         btnCancel.style.display = ""
+    } else {
+        btnConfirm.style.display = "none"
+        btnCancel.style.display = "none"
     }
-    // if (data.pos) {
-    //     if (data.pos == 1) {
-    //         user1.textContent = "User ID 1: " + data.userid
-    //     } else if (data.pos == 2) {
-    //         user2.textContent = "User ID 2: " + data.userid
-    //     }
-    // }
 }
 
 updateUI()
@@ -165,15 +217,15 @@ let boxPoll = () => {
     req.addEventListener('load', ev => {
         box = ev.target.response
         updateUI()
-        setTimeout(boxPoll, 5000)
+        setTimeout(boxPoll, 30000)
     })
     req.addEventListener('error', ev => {
         console.log(ev.target.response)
-        setTimeout(boxPoll, 5000)
+        setTimeout(boxPoll, 30000)
     })
     req.addEventListener('abort', ev => {
         console.log(ev.target.response)
-        setTimeout(boxPoll, 5000)
+        setTimeout(boxPoll, 30000)
     })
     let url = new URL(document.URL)
     req.open('POST', url.pathname)
